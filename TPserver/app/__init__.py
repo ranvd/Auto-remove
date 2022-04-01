824c69b13b696f3e959763de2eba82bb9a53bc9b
root <root@LapTop.localdomain>
2022-04-01 14:28:35 +0800

    server first page

--- TPserver/app/__init__.py
+++ TPserver/app/__init__.py
@@ -68,7 +68,7 @@ def create_app(test_config=None):
 
 
 '''
-'''
 if(__name__ == "__main__"):
     app = create_app()
     app.run()
+'''
---
+++ server/app/__init__.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from flask import Flask, url_for, render_template
+from flask_bootstrap import Bootstrap
+import json
+
+def create_app(Config_FileName = "config.json"):
+    app = Flask(__name__, instance_relative_config=True)
+    Bootstrap(app)
+
+    try:
+        app.config.from_file(Config_FileName, load=json.load)
+    except:
+        app.logger.error("Config file must be json format")
+        sys.exit("Error occur")
+    '''
+    for conf in app.config:
+        print(conf, " : ", app.config[conf])
+    '''
+    @app.route("/")
+    def root():
+        return render_template('base.html')
+
+    @app.route("/sidebar")
+    def sidebar():
+        return render_template('album.html')
+
+    @app.route("/album")
+    def album():
+        return render_template('sidebar.html')
+
+    @app.route("/index")
+    def index():
+        return render_template('index.html')
+
+    
+
+    return app
+
+'''
+if (__name__ == "__main__"):
+    app = create_app()
+'''
